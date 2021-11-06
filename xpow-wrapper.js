b9437e7500d5bd00e067004745160b04c27592e7
Sunur Efe Vural <efe@efe.kim>
2021-11-06 12:32:05 -0400

    improve code structure

--- xpow-wrapper.js
+++ xpow-wrapper.js
@@ -20,6 +20,14 @@ const contractAbi = [
 ]
 const xpowContract = new ethers.Contract(contractAddress, contractAbi, signer)
 
+const os = process.platform
+var minerExecutable
+if (os === 'linux') {
+	minerExecutable = './xpow-miner'
+} else if (os === 'win32') {
+	minerExecutable = 'xpow-miner.exe'
+}
+
 async function mint(nonce) {
 	await xpowContract
 		.mint(nonce)
@@ -35,13 +43,7 @@ async function mine() {
 	const interval = ethers.utils.formatUnits(await xpowContract.interval(),0)
 	const deadline = 3600 - ethers.utils.formatUnits(await xpowContract.deadline(),0)
 	const startingNonce = Math.floor((Math.random() * 10 ** 16 ) + 2 * 10 ** 16) // arbitrary random value
-	const os = process.platform
-	var miner
-	if (os === 'linux') {
-		miner = spawn('./xpow-miner', [startingNonce, walletAddress, interval, minWork, deadline])
-	} else if (os === 'win32') {
-		miner = spawn('xpow-miner.exe', [startingNonce, walletAddress, interval, minWork, deadline])
-	}
+	const miner = spawn(minerExecutable, [startingNonce, walletAddress, interval, minWork, deadline])
 	miner.stdout.on('data', (data) => {
 		const line = data.toString('utf-8')
 		if (line.startsWith("Good nonce:")) {
